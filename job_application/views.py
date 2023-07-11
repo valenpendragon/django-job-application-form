e55321efdf70b70d36741fe7cb3445c0080a26ad
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-11 03:17:43 -0400

    Add code to index view function to capture job form data. Add ApplicationForm class to new forms.py file to handle the job_application form data. Add code to insert form data into job_application data table. Add code to send confirmation email to user and to display a confirmation message on the form webpage when all this is done.

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        print(f"form: {form}")
+        print(f"form-validation: {form.is_valid()}")
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            occupation = form.cleaned_data["occupation"]
+            date = form.cleaned_data["date"]
+            print(f"first name: {first_name}")
+            print(f"last name: {last_name}")
+            print(f"email: {email}")
+            print(f"occupation: {occupation}")
+            print(f"date: {date}")
 
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                occupation=occupation, email=email,
+                                date=date)
+
+            message_body = f"A new job application was submitted.\n" \
+                           f"Thank you, \n{first_name}"
+            email_msg = EmailMessage("Form submission confirmation",
+                                     message_body, to=[email])
+            email_msg.send()
+
+            messages.success(request, "Form submitted successfully.")
     return render(request, "index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("GMAIL_ACCOUNT")
+EMAIL_HOST_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
