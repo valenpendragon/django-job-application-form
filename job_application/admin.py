0845f28d863cd9efdae2441a0726c2104ec3d6b0
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-12 03:40:19 -0400

    Add admin interface to job_application app.

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "email", "occupation", "date")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("last_name", "first_name")
+
+
+admin.site.register(Form, FormAdmin)
